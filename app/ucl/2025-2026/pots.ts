cb93c7da52ec708f59daf7b535ff6f5cca3efbad
LeoLucenaG08 <leo.lucena2008@gmail.com>
2026-05-24 23:20:50 +0200

    Update teams in UCL pots for 2025-2026

--- app/ucl/2025-2026/pots.ts
+++ app/ucl/2025-2026/pots.ts
@@ -1,46 +1,46 @@
 export const pots = [
     [
-        { "id": 0, "name": "Paris Saint-Germain", "country": "fr", },
-        { "id": 1, "name": "Real Madrid", "country": "es", },
-        { "id": 2, "name": "Man City", "country": "gb-eng", },
-        { "id": 3, "name": "Bayern München", "country": "de", },
-        { "id": 4, "name": "Liverpool", "country": "gb-eng", },
-        { "id": 5, "name": "Internazionale Milano", "country": "it", },
-        { "id": 6, "name": "Chelsea", "country": "gb-eng", },
-        { "id": 7, "name": "Borussia Dortmund", "country": "de", },
-        { "id": 8, "name": "FC Barcelona", "country": "es", }
+        { "id": 0, "name": "Arsenal FC", "country": "gb-eng", },
+        { "id": 1, "name": "Paris Saint-Germain", "country": "fr", },
+        { "id": 2, "name": "Real Madrid CF", "country": "es", },
+        { "id": 3, "name": "FC Bayern München", "country": "de", },
+        { "id": 4, "name": "FC Internazionale", "country": "it", },
+        { "id": 5, "name": "FC Barcelona", "country": "es", },
+        { "id": 6, "name": "Borussia Dortmund", "country": "de", },
+        { "id": 7, "name": "Atlético de Madrid", "country": "es", },
+        { "id": 8, "name": "Aston Villa FC", "country": "gb-eng", }
     ],
     [
-        { "id": 9, "name": "Arsenal", "country": "gb-eng", },
-        { "id": 10, "name": "Bayer Leverkusen", "country": "de", },
-        { "id": 11, "name": "Atlético de Madrid", "country": "es", },
-        { "id": 12, "name": "SL Benfica", "country": "pt", },
-        { "id": 13, "name": "Atalanta", "country": "it", },
-        { "id": 14, "name": "Villarreal", "country": "es", },
-        { "id": 15, "name": "Juventus", "country": "it", },
-        { "id": 16, "name": "Eintracht Frankfurt", "country": "de", },
-        { "id": 17, "name": "Club Brugge", "country": "be", }
+        { "id": 9, "name": "Manchester City FC", "country": "gb-eng", },
+        { "id": 10, "name": "Bayer 04 Leverkusen", "country": "de", },
+        { "id": 11, "name": "Real Betis Balompié", "country": "es", },
+        { "id": 12, "name": "Liverpool FC", "country": "gb-eng", },
+        { "id": 13, "name": "AS Roma", "country": "it", },
+        { "id": 14, "name": "SSC Napoli", "country": "it", },
+        { "id": 15, "name": "Club Brugge", "country": "be", },
+        { "id": 16, "name": "AS Mónaco", "country": "fr", },
+        { "id": 17, "name": "FC Porto", "country": "pt", }
     ],
     [
-        { "id": 18, "name": "Tottenham", "country": "gb-eng", },
-        { "id": 19, "name": "PSV Eindhoven", "country": "nl", },
-        { "id": 20, "name": "Ajax", "country": "nl", },
-        { "id": 21, "name": "Napoli", "country": "it", },
-        { "id": 22, "name": "Sporting CP", "country": "pt", },
-        { "id": 23, "name": "Olympiacos", "country": "gr", },
-        { "id": 24, "name": "Slavia Praha", "country": "cz", },
-        { "id": 25, "name": "Bodø/Glimt", "country": "no", },
-        { "id": 26, "name": "Marseille", "country": "fr", }
+        { "id": 18, "name": "Galatasaray SK", "country": "tu", },
+        { "id": 19, "name": "Ajax Ámsterdam", "country": "nl", },
+        { "id": 20, "name": "Celtic Glasgow FC", "country": "sc", },
+        { "id": 21, "name": "Feyenoord Rotterdam", "country": "nl", },
+        { "id": 22, "name": "Sparta Praha", "country": "cz", },
+        { "id": 23, "name": "Crystal Palace FC", "country": "gb-eng", },
+        { "id": 24, "name": "AC Milan", "country": "it", },
+        { "id": 25, "name": "Crvena Zvezda", "country": "sr", },
+        { "id": 26, "name": "AEK Atenas", "country": "gr", }
     ],
     [
-        { "id": 27, "name": "København", "country": "dk", },
-        { "id": 28, "name": "Monaco", "country": "fr", },
-        { "id": 29, "name": "Galatasaray", "country": "tr", },
-        { "id": 30, "name": "Union Saint-Gilloise", "country": "be", },
-        { "id": 31, "name": "Qarabağ", "country": "az", },
-        { "id": 32, "name": "Athletic Club", "country": "es", },
-        { "id": 33, "name": "New Castle", "country": "gb-eng", },
-        { "id": 34, "name": "Pafos", "country": "cy", },
-        { "id": 35, "name": "Kairat", "country": "kz", }
+        { "id": 27, "name": "Racing Club Lens", "country": "fr", },
+        { "id": 28, "name": "RSC Anderlecht", "country": "be", },
+        { "id": 29, "name": "Stade Rennais", "country": "fr", },
+        { "id": 30, "name": "Shamrock Rovers", "country": "ir", },
+        { "id": 31, "name": "Hoffenheim", "country": "de", },
+        { "id": 32, "name": "Dynamo Kiev", "country": "uc", },
+        { "id": 33, "name": "Lausanne Sport", "country": "sw", },
+        { "id": 34, "name": "Aberdeen", "country": "sc", },
+        { "id": 35, "name": "RCD Espanyol", "country": "es", }
     ]
 ]
